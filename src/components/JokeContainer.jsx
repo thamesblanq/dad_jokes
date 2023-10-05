bb94cef26929f332404b982f56735399e89123f9
blanq31 <obiwulugodswill@gmail.com>
2023-10-05 13:04:44 +0100

    code optimization

--- src/components/JokeContainer.jsx
+++ src/components/JokeContainer.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import axios from "axios";
 const baseUrl = 'https://jokes-api-0a9p.onrender.com/joke';
 
@@ -6,38 +6,10 @@ const baseUrl = 'https://jokes-api-0a9p.onrender.com/joke';
 const JokeContainer = () => {
     const [joke, setJoke] = useState(null);
     const [errorMsg, setErrorMsg] = useState(null);
-
-    useEffect(() => {
-            const getJoke = async () => {
-
-                const apiKey = (await axios.get(baseUrl)).data.message;
-
-
-                const options = {
-                  method: 'GET',
-                  url: 'https://dad-jokes.p.rapidapi.com/random/joke',
-                  headers: {
-                    'X-RapidAPI-Key': apiKey,
-                    'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
-                  }
-                };
-                
-                try {
-                    const response = await axios.request(options);
-                    setJoke(response.data.body[0])
-                } catch (error) {
-                    setInterval(() => {
-                        setErrorMsg(error.message)
-                    }, 5000)
-                    setErrorMsg("")
-                    console.error(error);
-                }
-            }
-            getJoke();
-    }, [])
-
+    const [loading, setLoading] = useState(false);
 
     const handleGetRandomJoke = async () => {
+        setLoading(true)
 
         const apiKey = (await axios.get(baseUrl)).data.message;
         const options = {
@@ -50,17 +22,24 @@ const JokeContainer = () => {
         };
         
         try {
+            setLoading(false);
             const response = await axios.request(options);
             setJoke(response.data.body[0])
         } catch (error) {
-            setInterval(() => {
-                setErrorMsg(error.message)
+            setErrorMsg(error.message)
+            setLoading(false)
+            setTimeout(() => {
+                setErrorMsg('')
             }, 5000)
-            setErrorMsg("")
+            console.log(errorMsg)
+
             console.error(error);
         }
 
     }
+
+
+
 
 
 
@@ -70,17 +49,20 @@ const JokeContainer = () => {
             <h1 className="font-onest text-4xl text-purple-500 italic font-extrabold">Dad Jokes</h1>
             <div className="flex items-center justify-center flex-col gap-y-6">
                 <div className="h-[30vh] w-[40vw] flex items-center justify-center border-2 border-purple-500 flex-col gap-y-2 rounded-lg">
-                    {joke ? (
+                    {loading && (<p className="text-xs md:text-sm font-semibold font-onest p-2 text-black pt-4">Loading...</p>)}
+                    {!loading && joke !== null && (
                         <>
                             <h2 className="text-xs md:text-sm font-semibold font-onest p-2 text-purple-400">{joke.setup}</h2>
                             <p className="text-xs md:text-sm font-onest p-2">{joke.punchline}</p>
                         </>
-                    ) : (
-                        <p className="text-xl md:text-2xl font-semibold font-onest text-red-500 p-2">Could not get Joke, try again</p>
-                    )}
+                    ) } 
+                        
+                    {!loading && (<p className="text-xl md:text-2xl font-semibold font-onest text-red-500 p-2">Could not get Joke, try again</p>)}
+                        
+
 
                 </div>
-                <p className="font-onest text-[2px] text-red-500">{errorMsg}</p>
+                {errorMsg && ( <p className="font-onest text-xs text-red-500">{errorMsg}</p> )}
                 <button className="bg-purple-500 text-white font-semibold font-onest text-sm px-6 py-3 rounded-lg" 
                 onClick={handleGetRandomJoke}>Generate Joke</button>
             </div>
